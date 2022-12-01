fd139b73d9ed7ebcdd63172a6300ac9b99ede379
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-01 23:23:52 +0300

    добавил переменные, alert и console.log

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = 'Good morning';
+let screens;
+const screenPrice = 599;
+let rollback;
+const fullPrice = 5000;
+let adaptive;
+
+alert('Наши научные труды достигли небывалой известности, поздравляю вас, товарищи!')
+console.log('Господа, отныне, это будет новая веха в нашей истории!');
