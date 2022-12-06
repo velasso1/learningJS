5886033df7414848f071a1b39b54f2a81edf8c66
Alexandr Vasin <alexandr.vasin1@yandex.ru>
2022-12-06 21:55:37 +0300

    доработка

--- script.js
+++ script.js
@@ -3,36 +3,65 @@
 const title = prompt('Как называется ваш проект?');
 const screens = prompt('Какие типы экранов нужно разработать?');
 const screenPrice = prompt('Сколько будет стоить данная работа?');
+
 const adaptive = alert('Нужен ли адаптив на сайте?');
+
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = prompt('Сколько это будет стоить?');
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice2 = prompt('Сколько это будет стоить?');
+
 const rollback = 58;
+
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = Math.ceil(fullPrice - (rollback / 100));
 
+let allServicePrices;
 
 
-if (fullPrice >= 30000) {
-    console.log('Даём скидку в 10%');
-} else if (fullPrice >= 15000) {
-    console.log('Даём скидку в 5%');
-} else {
-    console.log('Скидка не предусмотрена');
+
+
+
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return 'Даём скидку в 10%';
+    } else if (price >= 15000) {
+        return 'Даём скидку в 5%';
+    } else {
+        return 'Скидка не предусмотрена';
+    }
+};
+
+
+const getAllServicePrices = function (service1, service2) {
+    allServicePrices = service1 + service2;
+    return allServicePrices;
+};
+
+function getFullPrice(price1, price2) {
+    fullPrice = price1 + price2;
+    return fullPrice;
 }
 
+const getTitle = function (str) {
+    return str.charAt(0).toUpperCase();
+};
 
 
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+console.log(getRollbackMessage(fullPrice));
 
 console.log(screens.length);
 
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юаней`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юаней`);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 
 console.log(screens.toLowerCase(), screens.split(", "));
 
@@ -41,3 +70,9 @@ console.log(fullPrice * (rollback / 100));
 console.log(servicePercentPrice);
 
 
+
+const getTiftle = function (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+console.log(getTiftle('hello'));
